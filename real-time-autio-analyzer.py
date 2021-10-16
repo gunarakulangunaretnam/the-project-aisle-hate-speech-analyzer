fb7f738b6d7106c21173b54ced7c35326019b006
GunaRakulan-GR <gunarakulan@gmail.com>
2021-10-16 17:15:06 +0530

    Add speech_to_text method

--- real-time-autio-analyzer.py
+++ real-time-autio-analyzer.py
@@ -1,7 +1,7 @@
 import os
+import speech_recognition as sr
 
 knowledgebase = []
-
 
 def database_loader():
 
@@ -11,8 +11,25 @@ def database_loader():
 	    word = line.strip()
 	    knowledgebase.append(word)
 
+def speech_to_text():
 
+    while True:
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+	        print("Listening...")
+	       	r.pause_threshold = 1
+	        audio = r.listen(source)
+	        #audio = r.listen(source, phrase_time_limit=10) # Set listening time limit.
+
+	        try:
+	            text = r.recognize_google(audio)
+	            print("Recognizing...")  
+	            print(text)
+
+	        except Exception as e:
+	            print(e)
 
 
 if __name__ == "__main__": 
     database_loader()
+    speech_to_text()
