366b7cf5434a3578bdf79e42fed7364e1e2b46ad
GunaRakulan-GR <gunarakulan@gmail.com>
2021-11-09 04:28:29 +0530

    Get top ten tranding words

--- 03-basic-prototype-1/1-the-project-aisle-backend/app/Http/Controllers/PageController.php
+++ 03-basic-prototype-1/1-the-project-aisle-backend/app/Http/Controllers/PageController.php
@@ -19,74 +19,73 @@ class PageController extends Controller
 
     }
 
-    public function ViewDashboardPageController(){
+    public function TopTenKeywordsProcessing($Language){
 
-      date_default_timezone_set('Asia/Colombo');
+      date_default_timezone_set('Asia/Colombo'); // Set time zome as Colombo.
 
 
-      $MinDate = date('Y-m-d', strtotime('-30 day', strtotime(date('Y-m-d'))));
-      $MaxDate = date('Y-m-d');
+      $MinDate = date('Y-m-d', strtotime('-30 day', strtotime(date('Y-m-d'))));   //Get Current date and subtract 30 days from it.
+      $MaxDate = date('Y-m-d'); // Max date
 
-      $all_data = DB::select("SELECT * FROM processed_data WHERE analyzed_date BETWEEN '$MinDate' and '$MaxDate'");
+      $all_data = DB::select("SELECT * FROM processed_data, social_media WHERE processed_data.account_id = social_media.account_name AND social_media.language = '$Language' AND analyzed_date BETWEEN '$MinDate' AND '$MaxDate' AND result = '[HATE]'"); // SQL-CODE
 
-      $all_available_keywords = [];
-      $all_available_keywords_with_duplicate = [];
-      $all_available_keywords_with_length = [];
-      $all_available_keywords_with_length_filtered_top_ten = [];
+      $all_available_keywords = [];                 //  Store all keywords without duplication
+      $all_available_keywords_with_duplicate = []; //   Store all keywords with duplication
 
-      foreach ($all_data as $key => $data) {
+      $all_available_keywords_with_length = [];   //   Associative array that contains all keywords with length.
+      $all_available_keywords_with_length_filtered_top_ten = []; //Store filtered top ten keywords.
 
-        $keyword_list_str = str_replace("[", "", $data->hate_keywords);
-        $keyword_list_str = str_replace("]", "", $keyword_list_str);
+      foreach ($all_data as $key => $data) {          // Loop all data from datababse.
 
-        if (strpos($keyword_list_str, ',') == true) {
+        $keyword_list_str = str_replace("[", "", $data->hate_keywords);  // Remove [
+        $keyword_list_str = str_replace("]", "", $keyword_list_str);    //  Remove ]
 
-            $keyword_pieces = explode(",", $keyword_list_str);
-            #print_r ($keyword_pieces);
+        if (strpos($keyword_list_str, ',') == true) {          // Check whether the string contains (,). if it contains, that means it has multiple values.
 
-            foreach ($keyword_pieces as $key => $peice) {
+            $keyword_pieces = explode(",", $keyword_list_str);  // Split the string  by comma
 
-              if(in_array(trim($peice), $all_available_keywords) == false){
+            foreach ($keyword_pieces as $key => $peice) {      // Loop the pieces
 
-                array_push($all_available_keywords,trim($peice));
+              if(in_array(trim($peice), $all_available_keywords) == false){  // all_available_keywords does not contain, enter the keyword. This is to avoid duplication.
+
+                array_push($all_available_keywords,trim($peice));   // Push the keyword to all_available_keywords array.
 
               }
 
-              array_push($all_available_keywords_with_duplicate,trim($peice));
+              array_push($all_available_keywords_with_duplicate,trim($peice));  // No filter here, therefore, all keywords will be enterd with dulication.
 
             }
 
+        }else{ // if no comma, that means, there is only one element.
 
-        }else{
+          if(in_array(trim($keyword_pieces), $all_available_keywords) == false){ // Check if it already in the all_available_keywords. if does not, then
 
-          if(in_array(trim($keyword_pieces), $all_available_keywords) == false){
-
-            array_push($all_available_keywords,trim($keyword_pieces));
+            array_push($all_available_keywords,trim($keyword_pieces));  // Add the keyword to the list.
 
           }
 
-          array_push($all_available_keywords_with_duplicate,trim($peice));
+          array_push($all_available_keywords_with_duplicate,trim($peice));  // Add the keyword to the duplication array.
 
         }
       }
 
 
-      foreach ($all_available_keywords as $key => $data) {
+      foreach ($all_available_keywords as $key => $data) {     // Loop the all_available_keywords that does not have dulications.
 
-        $temp_data = $data;
-        $temp_length = 0;
+        $temp_data = $data;   // Store one keyword to the tamp variable.
+        $temp_length = 0;    // Create a variable as 0.
 
-        foreach ($all_available_keywords_with_duplicate as $key => $sub_data) {
+        foreach ($all_available_keywords_with_duplicate as $key => $sub_data) {  // Loop all_available_keywords_with_duplicate to check how many times that particalr keyword exist.
 
-          if($temp_data == $sub_data){
+          if($temp_data == $sub_data){ // If the temp keyword is equal to the $sub_data keyword.
 
-            $temp_length = $temp_length + 1;
+            $temp_length = $temp_length + 1; // Then increase by one.
 
           }
 
         }
 
-        $all_available_keywords_with_length[$temp_data] = $temp_length;
+        $all_available_keywords_with_length[$temp_data] = $temp_length; // Store keyword with length Ex: Apple 2
 
       }
 
@@ -94,33 +93,56 @@ class PageController extends Controller
 
       $number_of_times_to_run = 0;
 
-      if(count($all_available_keywords_with_length) < 10){
+      if(count($all_available_keywords_with_length) < 10){ // If all_available_keywords_with_length less than 10, then number of times to run is as usual.
 
         $number_of_times_to_run = count($all_available_keywords_with_length);
 
-      }else{
+      }else{ // Otherwise, it is ten.
 
         $number_of_times_to_run = 10;
 
       }
 
+      $total_number_of_keywords = 0;
 
-      for ($i = 1; $i <= $number_of_times_to_run; $i++) {
 
-        $key = array_search(max($all_available_keywords_with_length), $all_available_keywords_with_length);
+      for ($i = 1; $i <= $number_of_times_to_run; $i++) { // Loop based on number_of_times_to_run
+
+        $key = array_search(max($all_available_keywords_with_length), $all_available_keywords_with_length);  // Get max number, then get the key of it.
         $all_available_keywords_with_length_filtered_top_ten[$key] = $all_available_keywords_with_length[$key];
-        unset($all_available_keywords_with_length[$key]);
+
+        $total_number_of_keywords = $total_number_of_keywords + $all_available_keywords_with_length[$key]; // Add total keywords to calculate the percentage.
+
+        unset($all_available_keywords_with_length[$key]);  // Remove the max to find the next max number
 
       }
 
-      print_r($all_available_keywords_with_length_filtered_top_ten);
+      $final_array = [];
+
+      foreach ($all_available_keywords_with_length_filtered_top_ten as $key => $data) {  // Checking percentage
+
+        $per = ($data / $total_number_of_keywords) * 100;
+
+        array_push($final_array,[$key, $data,$per]);
+
+      }
 
 
+      return [$final_array];
+
+  }
+
+    public function ViewDashboardPageController(){
+
+      $SinhalaKeywordsData = $this->TopTenKeywordsProcessing("Sinhala");
+      $TamilKeywordsData   = $this->TopTenKeywordsProcessing("Tamil");
+
+    
       $session_type = Session::get('Session_Type');
 
       if($session_type == "Admin"){
 
-        return view('dashboard/home-page');
+        return view('dashboard/home-page')->with(["SinhalaKeywordsData" => $SinhalaKeywordsData]);
 
       }else{
 
