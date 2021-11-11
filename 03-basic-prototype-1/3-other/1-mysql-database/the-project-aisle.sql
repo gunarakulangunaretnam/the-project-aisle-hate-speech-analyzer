f8e2d26afa37def91d7f19edd13c9e8211f47829
GunaRakulan-GR <gunarakulan@gmail.com>
2021-11-11 19:38:17 +0530

    Upload database

--- 03-basic-prototype-1/3-other/1-mysql-database/the-project-aisle.sql
+++ 03-basic-prototype-1/3-other/1-mysql-database/the-project-aisle.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2021 at 12:20 PM
+-- Generation Time: Nov 11, 2021 at 03:07 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -33,6 +33,16 @@ CREATE TABLE `context_data` (
   `description` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `context_data`
+--
+
+INSERT INTO `context_data` (`auto_id`, `context`, `description`) VALUES
+(10, 'Gender', '[NULL]'),
+(11, 'LGBT', '[NULL]'),
+(12, 'Color', '[NULL]'),
+(13, 'religion', '[NULL]');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +57,15 @@ CREATE TABLE `knowledgebase` (
   `language` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `knowledgebase`
+--
+
+INSERT INTO `knowledgebase` (`auto_id`, `keyword`, `context_tags`, `description`, `language`) VALUES
+(15, 'ලොක්කො', '[\"Gender\",\"LGBT\"]', '[NULL]', 'Sinhala'),
+(16, 'ගෙදර', '[\"Gender\",\"religion\"]', '[NULL]', 'Sinhala'),
+(17, 'ගියෙ', '[\"LGBT\"]', '[NULL]', 'Sinhala');
+
 -- --------------------------------------------------------
 
 --
@@ -55,14 +74,30 @@ CREATE TABLE `knowledgebase` (
 
 CREATE TABLE `processed_data` (
   `auto_id` int(200) NOT NULL,
+  `account_id` varchar(10000) NOT NULL,
   `target_data` mediumtext NOT NULL,
   `hate_keywords` mediumtext NOT NULL,
-  `analyzed_date` varchar(100) NOT NULL,
+  `analyzed_date` date NOT NULL,
   `analyzed_time` varchar(100) NOT NULL,
   `processed_type` varchar(100) NOT NULL,
-  `result` varchar(100) NOT NULL,
-  `Account Name` varchar(1000) NOT NULL
+  `result` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `processed_data`
+--
+
+INSERT INTO `processed_data` (`auto_id`, `account_id`, `target_data`, `hate_keywords`, `analyzed_date`, `analyzed_time`, `processed_type`, `result`) VALUES
+(73, '', 'Project Files', '[]', '2021-11-11', '18:34:57', '[TEXT-DATA]', '[NON-HATE]'),
+(74, 'Lella Hutan', 'අනිත්', '[]', '2021-11-11', '18:35:38', '[TEXT-DATA]', '[NON-HATE]'),
+(75, 'Lella Hutan', 'අනිත්', '[]', '2021-11-11', '18:35:39', '[TEXT-DATA]', '[NON-HATE]'),
+(76, 'Lella Hutan', 'අනිත් ලොක්කො පාරෙ රංඩුත් වෙලා ගෙදර ගියෙ 😂', '[ලොක්කො, ගෙදර, ගියෙ]', '2021-11-11', '18:39:56', '[TEXT-DATA]', '[HATE]'),
+(77, 'Lella Hutan', 'අනිත් ලොක්කො පාරෙ රංඩුත් වෙලා ගෙදර ගියෙ 😂', '[ලොක්කො, ගෙදර, ගියෙ]', '2021-11-11', '18:40:03', '[TEXT-DATA]', '[HATE]'),
+(78, 'Lella Hutan', 'ගුරුවරු අස්සට උඹලෑ තාත්තා ගියේ මොකද ? උන් බනින්නේ බොලෑ තාත්තට', '[]', '2021-11-11', '18:40:47', '[TEXT-DATA]', '[NON-HATE]'),
+(79, 'Lella Hutan', 'අනිත් ලොක්කො පාරෙ රංඩුත් වෙලා ගෙදර ගියෙ 😂', '[ලොක්කො, ගෙදර, ගියෙ]', '2021-11-11', '18:40:55', '[TEXT-DATA]', '[HATE]'),
+(80, 'Lella Hutan', 'ලොක්කො', '[ලොක්කො]', '2021-11-11', '18:42:01', '[TEXT-DATA]', '[HATE]'),
+(81, 'Lella Hutan', 'ලොක්කො', '[ලොක්කො]', '2021-11-11', '18:42:08', '[TEXT-DATA]', '[HATE]'),
+(82, 'Lella Hutan', 'ireactsr', '[]', '2021-11-11', '18:46:57', '[TEXT-DATA]', '[NON-HATE]');
 
 -- --------------------------------------------------------
 
@@ -94,7 +129,8 @@ INSERT INTO `social_media` (`auto_id`, `social_media`, `account_name`, `account_
 (4, 'Facebook', 'Sampath Athukorala', 'Page', 'https://www.facebook.com/sampathathukoralaofficialpage/', '\r\n119145', '[NULL]', 'Sinhala', '[NULL]', '0'),
 (5, 'Twitter', 'Sampath Athukorala', 'Page', 'https://www.facebook.com/sampathathukoralaofficialpage/', '\r\n119145', '[NULL]', 'Sinhala', '[NULL]', '0'),
 (6, 'Twitter', 'Sampath Athukorala', 'Page', 'https://www.facebook.com/sampathathukoralaofficialpage/', '\r\n119145', '[NULL]', 'Sinhala', '[NULL]', '0'),
-(7, 'Twitter', 'Sampath Athukorala', 'Page', 'https://www.facebook.com/sampathathukoralaofficialpage/', '\r\n119145', '[NULL]', 'Sinhala', '[NULL]', '0');
+(7, 'Twitter', 'Page', 'Page', 'https://www.facebook.com/sampathathukoralaofficialpage/', '11914', '[NULL]', 'Sinhala', '[NULL]', '0'),
+(9, 'Facebook', 'Sri Lankan Blood - 1', 'Page', 'https://www.facebook.com/SriLankanBlood1/', '31158', '[NULL]', 'Sinhala', '[NULL]', '0');
 
 -- --------------------------------------------------------
 
@@ -113,7 +149,7 @@ CREATE TABLE `user_account` (
 --
 
 INSERT INTO `user_account` (`auto_id`, `username`, `password`) VALUES
-(1, 'admin', '$2y$10$/TMOH3H.sJbyEuA3ee2FUOEKCTZwk1QZIrDRnZMr51rsLDXtfR3Yy');
+(1, 'admin', '$2y$10$4SrAzmWiQDktwbqCu4h6uuahPXvncyK8GVYcYVi5lQAaAs1kti/wS');
 
 --
 -- Indexes for dumped tables
@@ -157,25 +193,25 @@ ALTER TABLE `user_account`
 -- AUTO_INCREMENT for table `context_data`
 --
 ALTER TABLE `context_data`
-  MODIFY `auto_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `auto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `knowledgebase`
 --
 ALTER TABLE `knowledgebase`
-  MODIFY `auto_id` int(200) NOT NULL AUTO_INCREMENT;
+  MODIFY `auto_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `processed_data`
 --
 ALTER TABLE `processed_data`
-  MODIFY `auto_id` int(200) NOT NULL AUTO_INCREMENT;
+  MODIFY `auto_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `social_media`
 --
 ALTER TABLE `social_media`
-  MODIFY `auto_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `auto_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_account`
