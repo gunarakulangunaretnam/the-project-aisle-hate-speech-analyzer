f65cea8e1f452e5fc1e4bf0aa776c7185a95c924
GunaRakulan-GR <gunarakulan@gmail.com>
2022-02-18 19:51:59 +0530

    Cleanup files

--- 3-other/2-important-code-snippets/0-copy-to-clipboard-code-example.py
+++ 3-other/2-important-code-snippets/0-copy-to-clipboard-code-example.py
@@ -1,11 +1,7 @@
 from pynput.keyboard import Key, Listener
 import pyperclip
 import time
-from happytransformer import HappyTextClassification
 
-
-
-happy_tc = HappyTextClassification("BERT", "Hate-speech-CNERG/dehatebert-mono-english")
 
 def on_press(key):
     #print('{0} pressed'.format(key))
@@ -13,14 +9,8 @@ def on_press(key):
     if str(key).strip() == "'\\x03'":
         time.sleep(1)
         copyText = pyperclip.paste()
-        result = happy_tc.classify_text(copyText)
-        print(result)
-        f = open("data.txt","w")
-        f.write(copyText+"\n")
-        f.write(result.label)
-        f.close()
+        print(copyText)
 
-        
 
 def on_release(key):
     #print('{0} release'.format(key))
